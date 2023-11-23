085cd512e92287bf652161b9dade4289ae2de90b
Bahir Hussain <mbahirh624@gmail.com>
2023-11-23 19:42:55 +0530

    Update

--- Java_Sources/Find_Missing_Number_Array/Find_Missing_Number_using_array_add.java
+++ Java_Sources/Find_Missing_Number_Array/Find_Missing_Number_using_array_add.java
@@ -1,21 +1,19 @@
 package Find_Missing_Number_Array;
 
 public class Find_Missing_Number_using_array_add {
-    public static int missingnumber(int []a, int l){
-
-        int sum = 0;
+    public static int missingnumber(int []a, int l, int ll){
+        int sum =0;
         for(int i=0; i<l; i++){
             sum = sum + a[i];
         }
-        return l*(l+1)/2 - sum;
+        return ll*(ll+1)/2 - sum;
     }
 
     public static void main(String[] args){
-
-
-        int a[] = {1, 2, 4, 5};
+        int a[] = {1,2,4,5};
         int l = a.length;
-        int ans = missingnumber(a, l);
-        System.out.println("Missing Number is " + ans);
+        int ll = 5;
+        int ans = missingnumber(a, l, ll);
+        System.out.println(ans);
     }
 }
