046c808db355a4a2a247dfb67fe22da09e6ea42b
Bahir Hussain <mbahirh624@gmail.com>
2023-11-23 19:45:44 +0530

    Update

--- Java_Sources/Move_zeros_end/move_zeros_end_BFA.java
+++ Java_Sources/Move_zeros_end/move_zeros_end_BFA.java
@@ -28,6 +28,13 @@ public class move_zeros_end_BFA {
         }
         return a;
     }
-
-
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
 }
