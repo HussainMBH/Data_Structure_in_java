e0fa80296cbeca71ea695c42f82ee88d499f437f
Bahir Hussain <mbahirh624@gmail.com>
2023-11-25 10:14:10 +0530

    Replace Element

--- Java_Sources/Replace_First/replace_first_element.java
+++ Java_Sources/Replace_First/replace_first_element.java
@@ -1,5 +1,9 @@
 package Replace_First;
 
 public class replace_first_element {
-
+    public static void main(String [] args){
+        String name = "Mohamed Bahir Hussain";
+        System.out.println("Befor Replace: " + name);
+        System.out.println("After Replace: " + name.replace("Mohamed", "Ahamed"));
+    }
 }
