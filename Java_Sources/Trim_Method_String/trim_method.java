8a7a00cf0348b41454bf1f0a8251b0c9791c3858
Bahir Hussain <mbahirh624@gmail.com>
2023-11-25 10:17:48 +0530

    Trim Method

---
+++ Java_Sources/Trim_Method_String/trim_method.java
@@ -0,0 +1,9 @@
+package Trim_Method_String;
+
+public class trim_method {
+    public static void main(String[] args){
+        String name = "      Bahir Hussain     ";
+        System.out.println("Before Trim: " + name);
+        System.out.println("After Trim: " + name.trim());
+    }
+}
