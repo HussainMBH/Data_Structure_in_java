a66fdcb3dc50c07e59318e045cbd384e4f65815e
Bahir Hussain <mbahirh624@gmail.com>
2023-11-26 13:04:41 +0530

    String to uppercase Manually

--- Java_Sources/Change_to_UpperClass/String_to_Uppercase.java
+++ Java_Sources/Change_to_UpperClass/String_to_Uppercase.java
@@ -5,5 +5,22 @@ public class String_to_Uppercase {
         String s = "vskjhsfb125@&%$sdsv";
         s = s.toUpperCase();
         System.out.println(s);
+
+        String_to_Uppercase obj = new String_to_Uppercase();
+        obj.changetouppercase();
+    }
+
+    public void changetouppercase(){
+        String s = "vskjhsfb125@&%$sdsv";
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch>= 'a' && ch<= 'z'){
+                ch = (char) (ch-32); // type casting
+                System.out.print(ch);
+            }
+            else {
+                System.out.print(ch);
+            }
+        }
     }
 }
