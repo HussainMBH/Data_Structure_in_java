16449e1072367c32fd3dc2deb4ff122b7ed6211d
Bahir Hussain <mbahirh624@gmail.com>
2023-11-27 15:51:58 +0530

    Substring

---
+++ Java_Sources/SubString_Method/Substring_method.java
@@ -0,0 +1,9 @@
+package SubString_Method;
+
+public class Substring_method {
+    public static void main(String[] args){
+        String s = "abcdefghijklmnopqrstuvwxyz";
+        s = s.substring(5, 10);
+        System.out.println(s);
+    }
+}
