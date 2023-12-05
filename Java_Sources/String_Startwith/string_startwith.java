dea97a56d31f2bfffd4c44bc52f448eb9c8398e6
Bahir Hussain <mbahirh624@gmail.com>
2023-12-05 11:46:09 +0530

    Strings Startswith check

--- Java_Sources/String_Startwith/string_startwith.java
+++ Java_Sources/String_Startwith/string_startwith.java
@@ -6,5 +6,27 @@ public class string_startwith {
         String b = "Hello";
         boolean result = a.startsWith(b);
         System.out.println(result);
+
+        string_startwith  obj = new string_startwith();
+        obj.stringwordcheck();
+    }
+
+    void stringwordcheck(){
+        String a = "Hello Mohamed Bahir Hussain";
+        String b = "Hello Mohamed";
+        int count  = 0;
+       for(int i=0; i<= b.length() - 1; i++){
+           if(a.charAt(i) == b.charAt(i)){
+               count++;
+               continue;
+           }
+           else {
+               System.out.println("Both Strings are not same");
+               break;
+           }
+       }
+       if(count == b.length()){
+           System.out.println("Both strings are same");
+       }
     }
 }
