b04f36328713d43424022500689230889fd32a7e
Bahir Hussain <mbahirh624@gmail.com>
2023-12-05 10:54:28 +0530

    String start with

---
+++ Java_Sources/String_Startwith/string_startwith.java
@@ -0,0 +1,10 @@
+package String_Startwith;
+
+public class string_startwith {
+    public static void main(String[] args) {
+        String a = "Hello Mohamed Bahir Hussain";
+        String b = "Hello";
+        boolean result = a.startsWith(b);
+        System.out.println(result);
+    }
+}
