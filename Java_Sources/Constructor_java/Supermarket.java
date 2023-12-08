f863af4c52dc1f88302c839117614bdf480ed87c
Bahir Hussain <mbahirh624@gmail.com>
2023-12-08 19:15:12 +0530

    Constructor

--- Java_Sources/Constructor_java/Supermarket.java
+++ Java_Sources/Constructor_java/Supermarket.java
@@ -5,7 +5,7 @@ public class Supermarket {
     String name;
     int price, discount;
     public Supermarket(String name, int price, int discount){
-        this.name = name;
+        this.name = name; // with this is a global variable, without this is a Local Variable
         this.price = price;
         this.discount = discount;
 //        System.out.println("This product name is " + name);
@@ -15,6 +15,12 @@ public class Supermarket {
     public static void main(String[] args) {
         Supermarket product1 = new Supermarket("Biscuits", 50, 8);
         product1.sell();
+        product1.giveback();
+    }
+
+    public void giveback(){
+        System.out.println("You bought " + name + ".");
+        System.out.println("That price is " + price + " with " + discount + " Rupees Discount.");
     }
 
     public void sell(){
