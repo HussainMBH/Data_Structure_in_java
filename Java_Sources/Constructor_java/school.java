c251debe8792ec63d17c8a979ccf9c07eb53843f
Bahir Hussain <mbahirh624@gmail.com>
2023-12-16 17:01:53 +0530

    Update

--- Java_Sources/Constructor_java/school.java
+++ Java_Sources/Constructor_java/school.java
@@ -1,4 +1,20 @@
 package Constructor_java;
 
 public class school {
+    String name;
+    int age;
+    public school(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        school firststudent = new school("Bahir", 23);
+        firststudent.showname();
+    }
+
+    void showname(){
+        System.out.println("Student name is " + name);
+        System.out.println("Student age is " + age);
+    }
 }
