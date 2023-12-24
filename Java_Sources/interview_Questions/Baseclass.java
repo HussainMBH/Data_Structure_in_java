d7f2b603ebf92dfe593fb2c81ef57dae995aed8a
Bahir Hussain <mbahirh624@gmail.com>
2023-12-24 22:30:52 +0530

    update

--- Java_Sources/interview_Questions/Baseclass.java
+++ Java_Sources/interview_Questions/Baseclass.java
@@ -18,6 +18,11 @@ public class Baseclass {
     }
 
     public static void main(String[] args) {
-
+        Base x = new Base();
+        Base y = new Derived();
+        Derived z = new Derived();
+        DoPrint(x);
+        DoPrint(y);
+        DoPrint(z);
     }
 }
