614abf1d6f2614290fca1f84cb8fa4a8f009a12b
Bahir Hussain <mbahirh624@gmail.com>
2024-01-13 20:41:18 +0530

    Arraylist

---
+++ Java_Sources/Collection_Java/List/ArrayList/ArrayListtry.java
@@ -0,0 +1,14 @@
+package Collection_Java.List.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListtry {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(5);
+        al.add(10);
+        al.add(4);
+        al.add(3);
+        System.out.println(al);
+    }
+}
--- Java_Sources/Recursion/test1.java
+++ Java_Sources/Recursion/test1.java
@@ -2,14 +2,16 @@ package Recursion;
 
 public class test1 {
 
-    static void printno(int i){
-        System.out.println(i);
+    static void printno(int i, int total){
+        total = total+i;
         i++;
-        if(i<=15){
-            printno(i);
+        if(i<=5){
+            printno(i, total);
         }
+        System.out.println(total);
     }
     public static void main(String[] args) {
-        printno(8);
+        int total = 0;
+        printno(1, total);
     }
 }
