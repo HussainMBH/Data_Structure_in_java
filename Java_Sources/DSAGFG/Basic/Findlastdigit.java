da75b779eab5a76bd2e22886f0b9c13c5394a325
Bahir Hussain <mbahirh624@gmail.com>
2024-01-21 20:12:08 +0530

    Find Last Digit

--- Java_Sources/DSAGFG/Basic/Findlastdigit.java
+++ Java_Sources/DSAGFG/Basic/Findlastdigit.java
@@ -1,4 +1,19 @@
 package DSAGFG.Basic;
 
 public class Findlastdigit {
+    public static void main(String[] args) {
+        finddigit();
+    }
+
+    static void finddigit(){
+        long a = 3;
+        long b = 6;
+        long c = a;
+        for(int i=1; i<b; i++){
+            a = a*c;
+        }
+        System.out.println(a);
+        long bln = a%10;
+        System.out.println(bln);
+    }
 }
