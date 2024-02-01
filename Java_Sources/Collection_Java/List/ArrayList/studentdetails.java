ea6528aa26a49c3d102fd7dd93a7d1b45094d6bb
Bahir Hussain <mbahirh624@gmail.com>
2024-02-01 20:54:58 +0530

    Update

--- Java_Sources/Collection_Java/List/ArrayList/studentdetails.java
+++ Java_Sources/Collection_Java/List/ArrayList/studentdetails.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class studentdetails {
    addstudent slist = new addstudent();
 
-//    public static void main(String[] args) {
-//        addname();
-//    }
-//
-//    static void addname(){
-//        Scanner scn = new Scanner(System.in);
-//        System.out.println("Enter your name and Grade");
-//        String name = scn.next();
-//        String grade = scn.next();
-//
-//        student s = new student(name, grade);
-//    }
+    public static void main(String[] args) {
+        addname();
+    }
+
+    static void addname(){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your name and Grade");
+        String name = scn.next();
+        String grade = scn.next();
+
+        student s = new student(name, grade);
+    }
 
     static void showname(){
 
