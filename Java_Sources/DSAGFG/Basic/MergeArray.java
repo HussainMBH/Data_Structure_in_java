aa311886b7332bcb40a673c98960c7b7406d04df
Bahir Hussain <mbahirh624@gmail.com>
2024-02-07 18:48:36 +0530

    Update

--- Java_Sources/DSAGFG/Basic/MergeArray.java
+++ Java_Sources/DSAGFG/Basic/MergeArray.java
@@ -1,6 +1,7 @@
 package DSAGFG.Basic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MergeArray {
     public static void main(String[] args) {
@@ -11,7 +12,6 @@ public class MergeArray {
             if(a[i] !=0){
                 ab.add(a[i]);
             }
-
         }
         for(int i=0; i<b.length; i++){
             if(b[i] !=0){
@@ -20,5 +20,20 @@ public class MergeArray {
         }
 
         System.out.println(ab);
+        mergarr();
+    }
+
+    static void mergarr(){
+        int a[] = {1,3,4,5,0,8,0,6};
+        int b[] = {7,2,4,6,0,4,0};
+        int k=0;
+        int v = 6;
+        for(int i=v;i<a.length;i++)
+        {
+            a[i]=b[k];
+            k++;
+        }
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
     }
 }
