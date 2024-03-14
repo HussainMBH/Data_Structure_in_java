bdd2b71e58b0fd7c85eabec895a6c806276a1f21
Bahir Hussain <mbahirh624@gmail.com>
2024-03-14 22:34:58 +0530

    Update

--- Java_Sources/Core_Java/Parameter_and_Arguments.java
+++ Java_Sources/Core_Java/Parameter_and_Arguments.java
@@ -8,7 +8,6 @@ public class Parameter_and_Arguments {
     }
 
     public static void main(String[] args) {
-        Parameter_and_Arguments objtest = new Parameter_and_Arguments();
-        System.out.println(objtest.addcalculat(10,20));
+
     }
 }
