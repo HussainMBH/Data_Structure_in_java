52021860cce274847dbbee300430b4876031379b
Bahir Hussain <mbahirh624@gmail.com>
2024-03-28 19:27:24 +0530

    String Question

---
+++ Java_Sources/Java_question/namestringqstn.java
@@ -0,0 +1,11 @@
+package Java_question;
+
+public class namestringqstn {
+    public static void main(String[] args) {
+        String a = "Bahir";
+        String b = "Hussain";
+
+        System.out.println(a = b);
+        System.out.println(a == b);
+    }
+}
