daf2d647ffd4e7e44eff02185c7c1ed2c3054ff9
Bahir Hussain <mbahirh624@gmail.com>
2024-04-04 10:56:20 +0530

    Update

--- Java_Sources/BasicDSA/Find_last_digit.java
+++ Java_Sources/BasicDSA/Find_last_digit.java
@@ -2,7 +2,7 @@ package BasicDSA;
 
 public class Find_last_digit {
     public static void main(String[] args) {
-        String a= "5";
+        String a= "8";
         String b = "4";
         int c = Integer.parseInt(a);
         int d = Integer.parseInt(b);
