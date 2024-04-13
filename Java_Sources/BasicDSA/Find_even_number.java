aa85fe36600fb99883b89305828c9a83fed4616b
Bahir Hussain <mbahirh624@gmail.com>
2024-04-13 10:56:20 +0530

    Update

--- Java_Sources/BasicDSA/Find_even_number.java
+++ Java_Sources/BasicDSA/Find_even_number.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 
 public class Find_even_number {
     public static void main(String[] args) {
-        int a[] = {5,8,9,4,3,6,1,2,7,10};
+        int a[] = {5,8,9,4,3,6,11,1,2,7,10};
         ArrayList<Integer> al = new ArrayList<>();
         for(int i=0; i<a.length; i++){
             if(a[i] % 2 == 0){
