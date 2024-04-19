5886d5da562445b0ad0f3522b3570bf58f9a9d31
Bahir Hussain <mbahirh624@gmail.com>
2024-04-19 06:21:21 +0530

    Update

---
+++ Java_Sources/Geeksforgeeks_Series/Array_Series/Square_root_of_number.java
@@ -0,0 +1,8 @@
+package Geeksforgeeks_Series.Array_Series;
+
+public class Square_root_of_number {
+    public static void main(String[] args) {
+        int x = 21;
+        System.out.println((int) Math.sqrt(x)); //SQRT find Square root of number method
+    }
+}
