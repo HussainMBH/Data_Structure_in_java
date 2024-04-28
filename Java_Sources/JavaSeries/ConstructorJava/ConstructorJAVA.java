62001eaebe18f189327bc41ad01b890852b6d477
Bahir Hussain <mbahirh624@gmail.com>
2024-04-28 11:10:59 +0530

    Constructor

--- Java_Sources/JavaSeries/ConstructorJava/ConstructorJAVA.java
+++ Java_Sources/JavaSeries/ConstructorJava/ConstructorJAVA.java
@@ -4,13 +4,21 @@ public class ConstructorJAVA {
 
     int a;
     String name;
-    public ConstructorJAVA(){
+
+    int age;
+    String secondname;
+    public ConstructorJAVA(int ag, String nm){
         a = 10;
         name = "Bahir";
+
+        age = ag;
+        secondname = nm;
     }
     public static void main(String[] args) {
-        ConstructorJAVA obj = new ConstructorJAVA(); //create object of Constructor class
+        ConstructorJAVA obj = new ConstructorJAVA(24, "Hussain"); //create object of Constructor class
         System.out.println(obj.a);
         System.out.println(obj.name);
+        System.out.println(obj.secondname);
+        System.out.println(obj.age);
     }
 }
