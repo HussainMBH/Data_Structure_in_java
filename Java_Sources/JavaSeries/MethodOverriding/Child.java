db6dfbdae946e619242f16dbcced9bf5f08c39f0
Bahir Hussain <mbahirh624@gmail.com>
2024-04-29 10:58:28 +0530

    Update

---
+++ Java_Sources/JavaSeries/MethodOverriding/Child.java
@@ -0,0 +1,8 @@
+package JavaSeries.MethodOverriding;
+
+public class Child extends Parent{
+    public static void main(String[] args) {
+        Child ch = new Child();
+        ch.get_admissiion();
+    }
+}
