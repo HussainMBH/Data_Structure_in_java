fb998b7b8d5f92a6028dffd682ef6678e5da95a6
Bahir Hussain <mbahirh624@gmail.com>
2024-05-25 11:40:58 +0530

    Update

--- Java_Sources/DSAGFG/Basic/FindMissingNumber.java
+++ Java_Sources/DSAGFG/Basic/FindMissingNumber.java
@@ -2,23 +2,22 @@ package DSAGFG.Basic;
 
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int a[] = {4,1,3,2,6,9,7,8,10};
-        int l = a.length;
-        int b = 0;
-        int ttl = 0;
-        for(int num : a){
-            b = b + num;
-        }
-        for(int i =1; i<a.length; i++){
-            ttl = ttl + i;
-        }
-
-        System.out.println(b);
-        int acrt = a.length + 1;
-        System.out.println(a.length);
-        System.out.println(acrt);
-        System.out.println(ttl);
-        System.out.println(ttl - acrt);
-
+        int[] numbers = {1, 2, 4, 5, 6}; // Example array
+        int missingNumber = findMissingNumber(numbers);
+        System.out.println("The missing number is: " + missingNumber);
     }
+
+    public static int findMissingNumber(int[] numbers) {
+        int n = numbers.length + 1; // Since one number is missing, the length of the complete array should be n+1
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : numbers) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+
 }
