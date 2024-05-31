3da7a0068e783df77be94fe1240147261a69b999
Bahir Hussain <mbahirh624@gmail.com>
2024-05-31 10:37:40 +0530

    Update

--- Java_Sources/Zoho_Coding_Test/FindcurrentTime.java
+++ Java_Sources/Zoho_Coding_Test/FindcurrentTime.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class FindcurrentTime {
     public static void main(String[] args) {
+        System.out.println("Find Current Date and Time...");
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(dtf.format(now));
@@ -15,5 +16,11 @@ public class FindcurrentTime {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         System.out.println(formatter.format(date));
+
+        System.out.println("Find Current Date...");
+        System.out.println(java.time.LocalDate.now());
+
+        System.out.println("Find Current Time...");
+        System.out.println(java.time.LocalTime.now());
     }
 }
