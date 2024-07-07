e9f1d7100139a0caf802750ab299d7863e8ac0c0
Bahir Hussain <mbahirh624@gmail.com>
2024-07-07 12:59:51 +0530

    Update

---
+++ Java_Sources/Collection_Java/List/ArrayList/AddLists.java
@@ -0,0 +1,15 @@
+package Collection_Java.List.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddLists {
+    public static void main(String[] args) {
+        var al = new ArrayList<>();
+        al.add("Mohamed");
+        al.add("Bahir");
+        al.add("Hussain");
+        al.add(3,"Software Engineer");
+        System.out.println(al);
+    }
+}
