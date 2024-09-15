8c9fd0d01b69a60bf1a86bb6fb62b20625eb31bd
Bahir Hussain <mbahirh624@gmail.com>
2024-09-15 15:24:26 +0530

    Update

---
+++ Java_Sources/MockInterview/Day01.java
@@ -0,0 +1,14 @@
+package MockInterview;
+
+public class Day01 {
+    public static final int a = 10;
+    static final public int b = 20;
+
+    private final int c = 25;
+
+    static public void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("hello");
+    }
+}
