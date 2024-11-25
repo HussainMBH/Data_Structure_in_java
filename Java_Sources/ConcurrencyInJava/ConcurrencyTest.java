b9eaabf81d87048c20cca0ea76634edafcc1abd3
Bahir Hussain <mbahirh624@gmail.com>
2024-11-25 12:25:14 +0530

    update

--- Java_Sources/ConcurrencyInJava/ConcurrencyTest.java
+++ Java_Sources/ConcurrencyInJava/ConcurrencyTest.java
@@ -4,7 +4,7 @@ public class ConcurrencyTest {
     public static void main(String[] args){
         //Creating two threads that execute separate tasks concurrency
         Thread thread1 = new Thread(new Task("Task 1"));
-        Thread thread2 = new Thread(new Task("Task "));
+        Thread thread2 = new Thread(new Task("Task 2"));
 
         // Starting both threads
         thread1.start();
